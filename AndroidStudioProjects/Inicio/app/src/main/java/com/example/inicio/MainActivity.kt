5701f616b92d5f92b93afc0bb4c83312bb23a88c
ch0rtas <47576091+ch0rtas@users.noreply.github.com>
2025-02-10 20:30:15 +0100

    Update

--- AndroidStudioProjects/Inicio/app/src/main/java/com/example/inicio/MainActivity.kt
+++ AndroidStudioProjects/Inicio/app/src/main/java/com/example/inicio/MainActivity.kt
@@ -1,33 +1,41 @@
 package com.example.inicio
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.CheckBox
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.inicio.databinding.ActivityMainBinding
-import com.google.android.material.snackbar.Snackbar
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var check: CheckBox;
-    private lateinit var button: Button;
+    private lateinit var butonLogin: Button;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding= ActivityMainBinding.inflate(layoutInflater)
         // setContentView(binding.root)
         setContentView(R.layout.activity_main)
 
         check = findViewById(R.id.checkInicio)
-        butonLogin = findViewById()
+        butonLogin = findViewById(R.id.btnLogin)
+
         acciones()
     }
 
     private fun acciones() {
-        check.set
+
+        check.setOnCheckedChangeListener { _, b ->
+            butonLogin.isEnabled = b
+        }
+
+        butonLogin.setOnClickListener {
+            // cambio de pantalla
+            val intent = Intent(applicationContext,SecondActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
