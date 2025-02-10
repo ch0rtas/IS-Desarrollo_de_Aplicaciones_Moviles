c64a776fa894d6834aca3180210c90b3fd7c2acd
ch0rtas <47576091+ch0rtas@users.noreply.github.com>
2025-02-10 12:28:58 +0100

    Update

--- AndroidStudioProjects/Inicio/app/src/main/java/com/example/inicio/MainActivity.kt
+++ AndroidStudioProjects/Inicio/app/src/main/java/com/example/inicio/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.inicio
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,18 +13,21 @@ import com.google.android.material.snackbar.Snackbar
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var check: CheckBox;
+    private lateinit var button: Button;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        // setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
-        binding.botonSaludar.setOnClickListener {
-            Snackbar.make(
-                binding.root,
-                "Enhorabuena ${binding.editNombre.text.toString()}",
-                Snackbar.LENGTH_LONG
-            ).show()
-        }
+        check = findViewById(R.id.checkInicio)
+        butonLogin = findViewById()
+        acciones()
+    }
+
+    private fun acciones() {
+        check.set
     }
 }
